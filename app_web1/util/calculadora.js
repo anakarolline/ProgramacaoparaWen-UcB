1ede02777e4b53c0af3d4fa76c063156f0199330
Ana Karolline <99661666+anakarolline@users.noreply.github.com>
2023-10-10 13:27:17 -0300

    Add files via upload

---
+++ app_web1/util/calculadora.js
@@ -0,0 +1,15 @@
+const calculadora = {
+  somar:function (a,b){
+    return a+b;
+    },
+    subtrair :function (a,b){
+      return a-b;
+      
+    },
+    multiplicar: function(a,b){
+      return a * b;
+    },
+    dividir:function(a,b){
+      return a/b;
+    }
+}
